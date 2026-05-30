e66e72bca8f6281fbe0b0f55e808865d2ce9acce
Shloka Konakalla <shlokakonakalla01@gmail.com>
2026-05-30 16:19:32 +0530

    Add hotel knowledgebase with pricing and ratings

---
+++ hotel_knowledgebase.py
@@ -0,0 +1,17 @@
+hotels = {
+
+    "Budget Inn": {
+        "price": 1500,
+        "rating": 4.0
+    },
+
+    "Comfort Stay": {
+        "price": 2500,
+        "rating": 4.3
+    },
+
+    "Luxury Palace": {
+        "price": 6000,
+        "rating": 4.8
+    }
+}
