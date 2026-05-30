906983129ba4b31e38ac9c40d910cce9faef16c5
Shloka Konakalla <shlokakonakalla01@gmail.com>
2026-05-30 16:17:48 +0530

    Add food knowledgebase with restaurant data

---
+++ food_knowledgebase.py
@@ -0,0 +1,20 @@
+restaurants = {
+
+    "Paradise": {
+        "cuisine": "Biryani",
+        "veg": False,
+        "cost": 500
+    },
+
+    "Saravana Bhavan": {
+        "cuisine": "South Indian",
+        "veg": True,
+        "cost": 300
+    },
+
+    "A2B": {
+        "cuisine": "South Indian",
+        "veg": True,
+        "cost": 250
+    }
+}
