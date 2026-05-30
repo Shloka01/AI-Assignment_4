5c80ad391fd773e6f0804adaea4e0d4bc0206188
Shloka Konakalla <shlokakonakalla01@gmail.com>
2026-05-30 16:21:02 +0530

    Create User class with initialization parameters

---
+++ userprofile.py
@@ -0,0 +1,12 @@
+class User:
+
+    def __init__(self,
+                 name,
+                 budget,
+                 interest,
+                 vegetarian):
+
+        self.name = name
+        self.budget = budget
+        self.interest = interest
+        self.vegetarian = vegetarian
