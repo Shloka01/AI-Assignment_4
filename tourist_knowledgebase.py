1df3c13849407550c76a4a4e49af5cf4d251d839
Shloka Konakalla <shlokakonakalla01@gmail.com>
2026-05-30 16:16:17 +0530

    Create tourist_knowledgebase.py

---
+++ tourist_knowledgebase.py
@@ -0,0 +1,31 @@
+tourist_places = {
+    "Charminar": {
+        "type": "Historical",
+        "city": "Hyderabad",
+        "cost": 100
+    },
+
+    "Golconda Fort": {
+        "type": "Historical",
+        "city": "Hyderabad",
+        "cost": 200
+    },
+
+    "Ramoji Film City": {
+        "type": "Entertainment",
+        "city": "Hyderabad",
+        "cost": 1500
+    },
+
+    "Marina Beach": {
+        "type": "Beach",
+        "city": "Chennai",
+        "cost": 0
+    },
+
+    "Mysore Palace": {
+        "type": "Historical",
+        "city": "Mysore",
+        "cost": 300
+    }
+}
